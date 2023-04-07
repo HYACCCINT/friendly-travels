29f1f609d2310bf4f428a53712f1698b9f0e7f26
Cynthia Wang <hyacccint@gmail.com>
2023-04-07 06:50:47 -0400

    feat: step1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,12 @@
 export const environment = {
     production: true,
     firebase: {
+      apiKey: "AIzaSyCClg3ylZvdSAYia4IMSvZHJ87PcA7H-oU",
+      authDomain: "friendly-travels-b6a4b.firebaseapp.com",
+      projectId: "friendly-travels-b6a4b",
+      storageBucket: "friendly-travels-b6a4b.appspot.com",
+      messagingSenderId: "798557434143",
+      appId: "1:798557434143:web:e8c8f0c14e277e6fd7802a",
+      measurementId: "G-G2EL53WG90"
     },
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,13 @@
 export const environment = {
   production: false,
   firebase: {
+    projectId: 'friendly-travels-b6a4b',
+    appId: '1:798557434143:web:e8c8f0c14e277e6fd7802a',
+    storageBucket: 'friendly-travels-b6a4b.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyCClg3ylZvdSAYia4IMSvZHJ87PcA7H-oU',
+    authDomain: 'friendly-travels-b6a4b.firebaseapp.com',
+    messagingSenderId: '798557434143',
+    measurementId: 'G-G2EL53WG90',
   },
 };
